39157a96164d690b25f4ac7fd91b19c7e2a6b377
BingqingLiu <31490879+BingqingLiu@users.noreply.github.com>
2019-04-07 23:34:51 +0100

    Update GARCH.py

--- GARCH.py
+++ GARCH.py
@@ -32,60 +32,26 @@ def Normalization(x,y):
     # y_full = np.log(y_full)
     return x_full, y_full
 
-# 2. Compute short term realized vol
-def Vol(y,vol_window):
-    Asset_vol1 = []
-    for window_number in range(0, len(y)):
-        Asset_temp = []
-        Asset_temp = y[window_number:vol_window + window_number]
-        Asset_temp = np.sqrt(sum(np.square(Asset_temp)) / len(Asset_temp))
-        Asset_vol1 = np.append(Asset_vol1, Asset_temp)
-    Asset_vol = (Asset_vol1-np.mean(Asset_vol1))/np.std(Asset_vol1)
-    return Asset_vol
-
 # -------------------------------------------------------------------------------------------
 # ----------------------Data Processing-------------------------------------
 # -------------------------------------------------------------------------------------------
 window_size = 50 #size of each window
 start_from = 0 #window starting position
 vol_window = 10
-# ---------------------------------------Positive GPR-----------------------------------------------------
+# ---------------------------------------GARCH-----------------------------------------------------
 
 # Data process
-Positive_y_full_EURCHF = [LogR_EURCHF_full[i] for i in range(0, n-1) if LogR_EURCHF_full[i]>0]
-Positive_x_full_EURCHF = [i*1.0 for i in range(0, n-1) if LogR_EURCHF_full[i]>0]
-[pos_x_full_EURCHF, pos_y_full_EURCHF] = Normalization(Positive_x_full_EURCHF,Positive_y_full_EURCHF)
+y_full_EURCHF = [LogR_EURCHF_full[i] for i in range(0, n-1) if LogR_EURCHF_full[i]!=0]
+x_full_EURCHF = [i*1.0 for i in range(0, n-1) if LogR_EURCHF_full[i]!=0]
+[x_EURCHF, y_EURCHF] = Normalization(x_full_EURCHF,y_full_EURCHF)
 
+y_full_EURNOK = [LogR_EURNOK_full[i] for i in range(0, n-1) if LogR_EURNOK_full[i]!=0]
+x_full_EURNOK = [i*1.0 for i in range(0, n-1) if LogR_EURNOK_full[i]!=0]
+[x_EURNOK, y_EURNOK] = Normalization(x_full_EURNOK,y_full_EURNOK)
 
-Positive_y_full_EURNOK = [LogR_EURNOK_full[i] for i in range(0, n-1) if LogR_EURNOK_full[i]>0]
-Positive_x_full_EURNOK = [i*1.0 for i in range(0, n-1) if LogR_EURNOK_full[i]>0]
-[pos_x_full_EURNOK, pos_y_full_EURNOK] = Normalization(Positive_x_full_EURNOK,Positive_y_full_EURNOK)
-
-
-Positive_y_full_EURSEK = [LogR_EURSEK_full[i] for i in range(0, n-1) if LogR_EURSEK_full[i]>0]
-Positive_x_full_EURSEK = [i*1.0 for i in range(0, n-1) if LogR_EURSEK_full[i]>0]
-[pos_x_full_EURSEK, pos_y_full_EURSEK] = Normalization(Positive_x_full_EURSEK,Positive_y_full_EURSEK)
-# # Volatility
-# pos_vol = Vol(pos_y_full,vol_window)
-
-# ---------------------------------------Negative GPR-----------------------------------------------------
-# Data process
-Negative_y_full_EURCHF = [-LogR_EURCHF_full[i] for i in range(0, n-1) if LogR_EURCHF_full[i]<0]
-Negative_x_full_EURCHF = [i*1.0 for i in range(0, n-1) if LogR_EURCHF_full[i]<0]
-[neg_x_full_EURCHF, neg_y_full_EURCHF] = Normalization(Negative_x_full_EURCHF,Negative_y_full_EURCHF)
-
-
-Negative_y_full_EURNOK = [-LogR_EURNOK_full[i] for i in range(0, n-1) if LogR_EURNOK_full[i]<0]
-Negative_x_full_EURNOK = [i*1.0 for i in range(0, n-1) if LogR_EURNOK_full[i]<0]
-[neg_x_full_EURNOK, neg_y_full_EURNOK] = Normalization(Negative_x_full_EURNOK,Negative_y_full_EURNOK)
-
-
-Negative_y_full_EURSEK = [-LogR_EURSEK_full[i] for i in range(0, n-1) if LogR_EURSEK_full[i]<0]
-Negative_x_full_EURSEK = [i*1.0 for i in range(0, n-1) if LogR_EURSEK_full[i]<0]
-[neg_x_full_EURSEK, neg_y_full_EURSEK] = Normalization(Negative_x_full_EURSEK,Negative_y_full_EURSEK)
-
-# Volatility
-# neg_vol = Vol(neg_y_full,vol_window)
+y_full_EURSEK = [LogR_EURSEK_full[i] for i in range(0, n-1) if LogR_EURSEK_full[i]!=0]
+x_full_EURSEK = [i*1.0 for i in range(0, n-1) if LogR_EURSEK_full[i]!=0]
+[x_EURSEK, y_EURSEK] = Normalization(x_full_EURSEK,y_full_EURSEK)
 
 
 # -------------------------------------------------------------------------------------------
@@ -110,26 +76,15 @@ def GARCH_pred(window_size,window_no,y_full):
 # -------------------------------------------------------------------------------------------
 # ----------------------section 4 Implementation-----------------------------
 # -------------------------------------------------------------------------------------------
-pos_window_number = 3000
-neg_window_number = 3000
+window_number = 3000
 
 # -----------------------------Positive GARCH Comparision------------------------------------
-pos_CHF_prediction = GARCH_pred(window_size,pos_window_number,pos_y_full_EURCHF)
-joblib.dump(pos_CHF_prediction, 'GARCH3000pos_EURCHF.pkl')
+CHF_prediction = GARCH_pred(window_size,window_number,y_full_EURCHF)
+joblib.dump(CHF_prediction, 'GARCH3000_EURCHF.pkl')
 
-pos_NOK_prediction = GARCH_pred(window_size,pos_window_number,pos_y_full_EURNOK)
-joblib.dump(pos_NOK_prediction, 'GARCH3000pos_EURNOK.pkl')
+NOK_prediction = GARCH_pred(window_size,window_number,y_full_EURNOK)
+joblib.dump(NOK_prediction, 'GARCH3000_EURNOK.pkl')
 
-pos_SEK_prediction = GARCH_pred(window_size,pos_window_number,pos_y_full_EURSEK)
-joblib.dump(pos_SEK_prediction, 'GARCH3000pos_EURSEK.pkl')
+SEK_prediction = GARCH_pred(window_size,window_number,y_full_EURSEK)
+joblib.dump(SEK_prediction, 'GARCH3000_EURSEK.pkl')
 
-
-# -----------------------------Negative GARCH Comparision------------------------------------
-neg_CHF_prediction = GARCH_pred(window_size,neg_window_number,neg_y_full_EURCHF)
-joblib.dump(neg_CHF_prediction, 'GARCH3000neg_EURCHF.pkl')
-
-neg_NOK_prediction = GARCH_pred(window_size,neg_window_number,neg_y_full_EURNOK)
-joblib.dump(neg_NOK_prediction, 'GARCH3000neg_EURNOK.pkl')
-
-neg_SEK_prediction = GARCH_pred(window_size,neg_window_number,neg_y_full_EURSEK)
-joblib.dump(neg_SEK_prediction, 'GARCH3000neg_EURSEK.pkl')
